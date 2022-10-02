8f4747c1d8468a1c8c2f21e0fc96f8a623e6d821
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-10-02 16:43:47 +0530

    commit

---
+++ components/Services.jsx
@@ -0,0 +1,13 @@
+import css from '../styles/Services.module.css'
+export default function Services(){
+    return(
+        <>
+        <div className={css.heading}>
+            <span>WHAT WE SERVE</span>
+            <span>Your Favourite Food</span>
+            <span>Delivery Partner</span>
+        </div>
+        {/* Features  */}
+        </>
+    )
+}
