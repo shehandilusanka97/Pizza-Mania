310e82fa7614f7c4ade4551eebc9521629323ad5
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-10-03 19:41:39 +0530

    commit

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import { urlFor } from '../lib/client'
+import Link from 'next/link'
 import css from '../styles/Menu.module.css'
 export default function Menu({pizzas}) {
-    console.log(pizzas)
+    
     return(
         <div className={css.container}>
             <div className={css.heading}>
@@ -16,10 +17,12 @@ export default function Menu({pizzas}) {
                 const src= urlFor(pizzas.image).url()
                 return(
                     <div className={css.pizza} key={id}>
+                        <Link href={`./pizza/${pizzas.slug.current}`}>
                       <div className={css.ImageWrapper}>
                         <Image loader={()=>src} src={src} alt=''
                         objectFit='cover' layout='fill'/>
                       </div>
+                      </Link>
                       <span>{pizzas.name}</span>
                       <span> <span style={{color:'var(--themeRed)'}}>Rs</span> {pizzas.price[1]}</span>
                     </div>
